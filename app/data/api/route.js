16ebdb72e94eafc9cdbf9f6dc630a82b829367db
Sahil Tambe <sahiltambe81922@gmail.com>
2023-08-16 14:56:10 +0530

    ssg not workiing

---
+++ app/data/api/route.js
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+import db from '../../../Data/db'
+export async function GET() {
+    console.log("api hitted")
+    const res = await db.find().toArray();
+    const meettups = res.map( meetup => ({id:meetup._id.toString() , title:meetup.title , image:meetup.image , description:meetup.description , address:meetup.address}))
+    return NextResponse.json(meettups)
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,78 +1,43 @@
-"use client";
 import MeetupList from "@/components/meetups/MeetupList";
-import db from "../Data/db";
+import { GetStaticProps } from "next";
 
-const meetups = [
-  {
-    id: "m1",
-    title: "meetup 1",
-    image:
-      "https://punetourism.co.in/images/places-to-visit/headers/top-tourist-places-to-visit-in-pune.jpg",
-    address: "isquareit",
-  },
-  {
-    id: "m2",
-    title: "meetup 2",
-    image:
-      "https://media1.thrillophilia.com/filestore/x498v676yykxnwf44q1ulgqrf8rj_wildernesthilltopresort-20230318-0003.jpg?w=305&h=230&dpr",
-    address: "isquareit nalanda",
-  },
-  {
-    id: "m3",
-    title: "meetup 3",
-    image:
-      "https://www.fabhotels.com/blog/wp-content/uploads/2018/09/Lakes-Dams-and-Hills.jpg",
-    address: "isquareit mithila",
-  },
-];
-
-import { useSelector } from "react-redux";
-interface obj {
+interface Meetup {
   id: string;
-  image: string;
   title: string;
+  image: string;
   address: string;
 }
 
-export default async function Home(props: { meetups: obj[] }) {
-  // const state:obj[] = useSelector((state:{meetup:obj[]})=> state.meetup)
-  // const data = await fetch("/api",{method:'GET'});
-  // const meetupsArr = await data.json();
-
-  const state = props.meetups || meetups;
-
-  return (
-    <main>
-      <MeetupList meetups={state} />
-    </main>
-  );
+interface HomeProps {
+  meetups: Meetup[];
 }
 
-// export async function getStaticProps() {
-//   console.log("working")
-//   const data = await fetch("/api",{method:'GET'});
-//   const meetups = await data.json();
-//   console.log(meetups)
-//   return{
-//     props:{
-//       meetups,
-//     },
-//   }
-// }
-export async function getStaticProps() {
-  console.log("working");
-  const res = await db.find().toArray();
-  const meetups = res.map((meetup) => ({
-    id: meetup._id.toString(),
-    title: meetup.title,
-    image: meetup.image,
-    description: meetup.description,
-    address: meetup.address,
-  }));
+export const getStaticProps: GetStaticProps<HomeProps> = async (context) => {
+  try {
+    console.log("Fetching meetups...");
+    const response = await fetch("/api", { method: "GET" });
+    const meetupsArr = await response.json();
 
-  return {
-    props: {
-      meetups,
-    },
-  };
-}
+    console.log("Fetched meetups:", meetupsArr);
+
+    return {
+      props: {
+        meetups: meetupsArr,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: {
+        meetups: [],
+      },
+    };
+  }
+};
+
+const Home = ({ meetups }: HomeProps) => {
+  console.log(meetups)
+  return <main>{meetups && <MeetupList meetups={meetups} />}</main>;
+};
+
+export default Home;
